f8fd839982284033a5041e0085c0c1dcd52731cd
Shihab US <shihabudheen@bounceshare.com>
2020-06-11 17:34:07 +0530

    Self signin works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const apiKey = "YOUR_API_KEY";
+const discoveryDocs = [
+  "https://people.googleapis.com/$discovery/rest?version=v1",
+];
+var clientId =
+  "1086442865500-dmepui1f6a652hfjn3pcgukma5lcqkgi.apps.googleusercontent.com";
+
+var scopes = "profile";
 
 function App() {
+  var auth2Instance = useRef(null);
+  useEffect(() => {
+    window.gapi.load("auth2", async function () {
+      const googleAuth = await window.gapi.auth2.init({
+        client_id:
+          "260896681708-o8bddcaipuisksuvb5u805vokq0fg2hc.apps.googleusercontent.com",
+      });
+      auth2Instance = window.gapi.auth2.getAuthInstance();
+      auth2Instance.isSignedIn.listen(updateSigninStatus);
+      updateSigninStatus(auth2Instance.isSignedIn.get());
+      console.log("googleAuth", auth2Instance);
+    });
+  }, []);
+
+  const onSignIn = (e) => {
+    e.preventDefault();
+    auth2Instance.signIn();
+  };
+
+  function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+      console.log("Signed In");
+      makeApiCall();
+    } else {
+      console.log("Not signed in");
+    }
+  }
+
+  function makeApiCall() {
+    window.gapi.client.people.people
+      .get({
+        resourceName: "people/me",
+        "requestMask.includeField": "person.names",
+      })
+      .then(function (resp) {
+        console.log("makeApiCall resp", resp);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onSignIn}>Sign in</button>
     </div>
   );
 }
